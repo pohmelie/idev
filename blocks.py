951e1204674f81930b00181ac6233b536fbec622
pohmelie <pohmelie@mail.ru>
2013-01-25 17:36:32 +0200

    * threading for logging

--- blocks.py
+++ blocks.py
@@ -3,6 +3,7 @@ from tkinter.ttk import *
 from tkinter.scrolledtext import ScrolledText
 from time import strftime
 from formats import Container
+from collections import deque
 
 
 class HorizontalButtons(Frame):
@@ -31,21 +32,26 @@ LoggerColors = Container(
             foreground='#ff7f7f'
         ),
         borring=Container(
-            foreground='#909294'  # blue -> '#ADD8E6'
+            foreground='#909294'
         ),
         time=Container(
             foreground='#93c763'
-        )
+        ),
+        event=Container(
+            foreground='#ADD8E6'
+        ),
     )
 )
 
 
 class Logger(Frame):
 
-    ERROR, BORRING, NORMAL = "error", "borring", None
+    ERROR, BORRING, EVENT, NORMAL = "error", "borring", "event", None
 
     def __init__(self, *args, **kw):
         Frame.__init__(self, *args, **kw)
+
+        self.deque = deque()
 
         self.txt = ScrolledText(self, wrap=WORD, state=DISABLED, relief=SUNKEN, height=25)
         self.txt.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -54,12 +60,24 @@ class Logger(Frame):
             self.txt.tag_configure(k, v)
 
         self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.after(100, self._toscreen)
 
-    def __call__(self, obj, tag=None):
-        self.txt["state"] = NORMAL
-        self.txt.insert(END, "\n[{}] ".format(strftime("%H:%M:%S")), "time", obj, tag)
-        self.txt.yview(END)
-        self.txt["state"] = DISABLED
+    def __call__(self, *args):
+        self.deque.appendleft(args)
+
+    def _toscreen(self):
+        if len(self.deque):
+            self.txt["state"] = NORMAL
+            while(len(self.deque)):
+                self.txt.insert(
+                    END,
+                    "\n[{}] ".format(strftime("%H:%M:%S")),
+                    "time",
+                    *self.deque.pop())
+            self.txt.yview(END)
+            self.txt["state"] = DISABLED
+        self.after(100, self._toscreen)
 
 if __name__ == "__main__":
     root = Tk()
@@ -70,12 +88,14 @@ if __name__ == "__main__":
         ("three", lambda: print("three")))
     ).grid(column=0, row=0, sticky=(N, W, E, S))
 
-    log = Logger()
-    log.grid(column=0, row=1, sticky=(N, W, E, S))
+    log = Logger(root)
+    log.grid(column=0, row=1, sticky=(W, E, S))
 
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
+    #root.rowconfigure(1, weight=0)
     log("normal")
     log("borring", Logger.BORRING)
     log("error", Logger.ERROR)
+    log("event", Logger.EVENT)
     root.mainloop()
